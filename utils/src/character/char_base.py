d60bc9c2bcc347eb75453d5dc0833b821109b643
EEsDoNotItNow <eesdonotitnow@gmail.com>
2019-04-20 20:38:50 -0600

    Cleanup character card

--- utils/src/character/char_base.py
+++ utils/src/character/char_base.py
@@ -183,10 +183,5 @@ class char_base:
         print(f"            Age: {self.age:.0f} years")
         print(f"         Weight: {self.weight:.0f} kg")
         print(f"     Profession: {self.profession}")
-        #print(f"Temperament: {'Nervous':>11s} [{self.slider_fill(self.temperament, 5)}] {'Relaxed':<11s} ({self.temperament:4.1f})")
-        #print(f"Law Abiding: {'Unlawful':>11s} [{self.slider_fill(self.law_abiding, 5)}] {'Lawful':<11s} ({self.law_abiding:4.1f})")
-        #print(f"Opinionated: {'Open Minded':>13s} [{self.slider_fill(self.opinionated, 5)}] {'Dogmatic':<11s} ({self.opinionated:4.1f})")
         for key, value in self.attributes.items():
             print(f"{key.title().replace('_',' '):>15s}: {value['neg'].title():>11s} [{self.slider_fill(value['score'], 7)}] {value['pos'].title():<11s} ({value['score']:4.1f})")
-
-            #print(key, value)
